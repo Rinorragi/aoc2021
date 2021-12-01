64ef2a5afb393c1a8e5b4129d0a0ee694a86282d
Rinorragi <rinorragi@gmail.com>
2021-12-01 09:02:58 +0200

    AoC2021: Day 1

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # aoc2021
+
+## Memo about setup
+
+What was required to put the repository up. [Download](https://dotnet.microsoft.com/download/dotnet/6.0) .NET 6 SDK.
+
+```powershell
+dotnet new gitignore
+```
+
+## How to run
+
+[Last year](https://github.com/Rinorragi/aoc2020) I did more solution approach. This year we shall go with fsharp scripts.
+
+```powershell
+dotnet fsi dayX.fsx
+```
---
+++ day1.fsx
@@ -0,0 +1,38 @@
+open System
+open System.IO
+
+let readlLines(filePath:string) = seq {
+    use sr = new StreamReader (filePath)
+    while not sr.EndOfStream do
+        yield sr.ReadLine ()
+}
+
+let compareSum (refList : int list, indx : int) =
+    refList.Item(indx-2) + refList.Item(indx-1) + refList.Item(indx) > refList.Item(indx-3) + refList.Item(indx-2) + refList.Item(indx-1)
+
+
+printfn "Advent of Code Day 1 - Part 1"
+let depthList : int list = 
+    readlLines "./input/input_day1.txt"
+    |> Seq.map System.Int32.Parse
+    |> List.ofSeq
+
+let increases = 
+    depthList
+    |> Seq.pairwise
+    |> Seq.filter (fun aDepth -> snd (aDepth) > fst (aDepth)) 
+
+let answer1 = increases |> Seq.length
+printfn "Answer 1: %d" answer1
+
+let increasesPart2 = 
+    depthList
+    |> List.mapi (fun i x -> if i > 2 then Some(compareSum (depthList, i)) else None)
+    |> List.filter (fun v -> Option.isSome v)
+    |> List.map Option.get
+    |> List.filter (fun x -> x = true)
+    
+let answer2 = increasesPart2 |> Seq.length
+printfn "Answer 2: %d" answer2
+
+
