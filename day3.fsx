5050f5ad8826288741f9f68139147650f31bc72f
Rinorragi <rinorragi@gmail.com>
2021-12-03 13:21:21 +0200

    AoC2021: Day 3 - part 2

--- day3.fsx
+++ day3.fsx
@@ -5,10 +5,10 @@ exception ValueProblem of string
 let commonBitCalculation (reading: string) =
     reading
     |> Seq.toList
-    |> List.mapi (fun i x -> 
+    |> List.map (fun x -> 
         match x with
-        | '0' -> (1, 0)
-        | '1' -> (0, 1)
+        | '0' -> 0
+        | '1' -> 1
         | _ -> raise(ValueProblem(sprintf "Value %c is not possible" x))
     )
 
@@ -19,8 +19,8 @@ let tupleToBit (state: (int * int)) =
         else
             1
 
-let bitArrayToInt (aBitArray: int list) =
-    aBitArray
+let bitArrayToInt (aBitList: int list) =
+    aBitList
     |> List.rev
     |> List.fold (fun acc elem ->
         // index, sum
@@ -29,41 +29,98 @@ let bitArrayToInt (aBitArray: int list) =
         (0, 0)
     |> snd 
 
+let calculateMostCommonBitList (aBitList: (int list) list) = 
+    let listLength = aBitList.Head.Length
+    aBitList
+    |> List.map (fun x -> 
+        x 
+        |> List.map (fun y -> 
+            match y with
+            | 0 -> (1, 0)
+            | 1 -> (0, 1)
+            | _ -> raise(ValueProblem(sprintf "Value %d is not possible" y))))
+    |> List.fold (fun acc elem ->     
+        elem 
+        |> List.mapi (fun i x -> fst acc[i] + fst x, snd acc[i] + snd x)) 
+        (List.ofArray (Array.create (listLength + 1) (0, 0)))
+    |> List.map tupleToBit
+
+let reverseBitList (bitList: int list) =
+    bitList 
+    |> List.map (fun x -> 
+    match x with 
+    | 0 -> 1
+    | 1 -> 0
+    | _  -> raise(ValueProblem(sprintf "Value %d is not possible" x)))
+
 printfn "Advent of Code Day 3"
 let commandList = 
     System.IO.File.ReadLines "./input/input_day3.txt"
     |> Seq.toList
 
-let valueLength = commandList.Head.Length
-let standardSizedArray = Array.create (valueLength + 1) (0, 0)
-
 // gamma rate = most common bit "in the corresponding position"
-let gammaRateBitArray =
+let gammaRateBitList =
     commandList
     |> List.map commonBitCalculation
-    |> List.fold (fun acc elem ->     
-        elem 
-        |> List.mapi (fun i x -> fst acc[i] + fst x, snd acc[i] + snd x)) (List.ofArray standardSizedArray)
-    |> List.map tupleToBit
+    |> (calculateMostCommonBitList)
 
 // epsilon rate = least common bit "in the corresponding position"
-let epsilonRateBitArray = 
-    gammaRateBitArray
-    |> List.map (fun x -> 
-        match x with 
-        | 0 -> 1
-        | 1 -> 0
-        | _  -> raise(ValueProblem(sprintf "Value %d is not possible" x)))
+let epsilonRateBiList = 
+    gammaRateBitList
+    |> reverseBitList
 
-printfn "Epsilon rate %A" epsilonRateBitArray
-
-let gammaRate = bitArrayToInt gammaRateBitArray
-let epsilonRate = bitArrayToInt epsilonRateBitArray
+let gammaRate = bitArrayToInt gammaRateBitList
+let epsilonRate = bitArrayToInt epsilonRateBiList
 
 // power consumption = gamma rate * epsilon rate
 let powerConsumption = gammaRate * epsilonRate
 
 printfn "Answer 1: %d"  powerConsumption
 
-//printfn "Answer 2: %d" answer2
+let commandListIntList = 
+    commandList 
+    |> List.map (fun f -> 
+        f.ToCharArray() 
+        |> Array.map (fun x -> 
+            match x with 
+            | '0' -> 0
+            | '1' -> 1
+            | _ -> raise(ValueProblem(sprintf "Value %c is not possible" x)))
+        |> List.ofArray)
+
+let valueLength = commandList.Head.Length
+// Oxygen rating filter by most common value until only 1 left
+let oxygenRatingList =
+    [0 .. 1 .. valueLength - 1]
+    |> List.fold (fun (acc: (int list) list) (elem: int) -> 
+        if acc.Length = 1
+        then 
+            acc
+        else 
+            let mostCommonBitList = calculateMostCommonBitList acc
+            acc 
+            |> List.filter (fun f -> f[elem] = mostCommonBitList[elem])
+    ) commandListIntList
+// CO2 Scrubber value filter by least common value until 1 left
+let co2ScrubberList =
+    [0 .. 1 .. valueLength - 1]
+    |> List.fold (fun (acc: (int list) list) (elem: int) -> 
+        if acc.Length = 1
+        then 
+            acc
+        else 
+            let leastCommonBitList = 
+                acc 
+                |> calculateMostCommonBitList
+                |> reverseBitList
+            acc 
+            |> List.filter (fun f -> f[elem] = leastCommonBitList[elem])
+    ) commandListIntList
+// Life support rating = oxygen rating * co2 scrubber
+let oxygenRating = bitArrayToInt oxygenRatingList.Head
+let co2ScrubberValue = bitArrayToInt co2ScrubberList.Head
+
+let lifeSupportRating = oxygenRating * co2ScrubberValue
+
+printfn "Answer 2: %d" lifeSupportRating 
 
