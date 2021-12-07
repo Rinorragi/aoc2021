a424bcf4e7e4400850b2bf27293ae9346b34d034
Rinorragi <rinorragi@gmail.com>
2021-12-07 08:35:26 +0200

    AoC2021: Day 7 - tidy up

--- day7.fsx
+++ day7.fsx
@@ -8,29 +8,24 @@ let crabMarines =
     |> Array.map int
     |> List.ofArray
 
-let minPos = crabMarines |> List.min
-let maxPos = crabMarines |> List.max
-
 let part1CalculationFunc (crabMarine: int) (pos: int) =
     crabMarine - pos |> abs
 
 let part2CalculationFunc (crabMarine: int) (pos: int) =
     let distance = part1CalculationFunc crabMarine pos
-    match distance with
-    | 0 -> 0
-    | x -> [0 .. x] |> List.sum
+    [0 .. distance] |> List.sum
 
 let calculateFuelUsageByPosition calculationFunc = 
+    let minPos = crabMarines |> List.min
+    let maxPos = crabMarines |> List.max
     [minPos .. maxPos]
     |> List.map (fun pos -> 
-        let fuelSum = 
-            crabMarines 
-            |> List.map (fun crabMarine -> calculationFunc crabMarine pos)
-            |> List.sum
-        (pos,fuelSum))
+        crabMarines 
+        |> List.map (fun crabMarine -> calculationFunc crabMarine pos)
+        |> List.sum)
 
-let answer = calculateFuelUsageByPosition part1CalculationFunc |> List.minBy snd |> snd
+let answer = calculateFuelUsageByPosition part1CalculationFunc |> List.min
 printfn "Answer 1: %d" answer
 
-let answer2 = calculateFuelUsageByPosition part2CalculationFunc |> List.minBy snd |> snd
+let answer2 = calculateFuelUsageByPosition part2CalculationFunc |> List.min
 printfn "Answer 2: %d" answer2
