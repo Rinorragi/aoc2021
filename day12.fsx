c4e1135c9c6c3d7996c076b1715b0601e7277b72
Rinorragi <rinorragi@gmail.com>
2021-12-12 20:11:26 +0200

    Day 12 - part 2

--- day12.fsx
+++ day12.fsx
@@ -63,29 +63,29 @@ let caves =
 let routeToString (route: Cave list) = 
     route |> List.map (fun r -> r.Id) |> List.fold (+) "->"
 
-let filterNextCaves (routeResults: Cave list list) (route: Cave list) (nextCave : Cave) = 
+let filterNextCaves smallCaveFilter (routeResults: Cave list list) (route: Cave list) (nextCave : Cave) = 
     if nextCave.Size = CaveSize.Start
     then false
-    elif nextCave.Size = CaveSize.Small && (route |> List.contains nextCave)
+    elif smallCaveFilter nextCave route
     then false
     elif routeResults |> List.map routeToString |> List.exists (fun f -> f.StartsWith(routeToString (route @ [nextCave])))
     then false
     else true
 
-let rec findPaths (routeResults: Cave list list) (route: Cave list) (currentCave : Cave) = 
+let rec findPaths smallCaveFilter (routeResults: Cave list list) (route: Cave list) (currentCave : Cave)  = 
     let newRoute = route @ [currentCave]
     if currentCave.Size = CaveSize.End
     then  routeResults @ [newRoute]
     else 
         let pathsToContinue =
             currentCave.Connections
-            |> List.filter (fun strNextCave -> filterNextCaves routeResults newRoute caves[strNextCave])
+            |> List.filter (fun strNextCave -> filterNextCaves smallCaveFilter routeResults newRoute caves[strNextCave])
 
         pathsToContinue 
         |> List.fold (fun newResults strNextCave -> 
-            if filterNextCaves routeResults newRoute caves[strNextCave]
+            if filterNextCaves smallCaveFilter routeResults newRoute caves[strNextCave]
             then  
-                let result = findPaths routeResults newRoute caves[strNextCave]
+                let result = findPaths smallCaveFilter routeResults newRoute caves[strNextCave]
                 result @ newResults
             else 
                 newResults
@@ -93,8 +93,19 @@ let rec findPaths (routeResults: Cave list list) (route: Cave list) (currentCave
         |> List.distinct
             
 
+let partOneFilter nextCave route = nextCave.Size = CaveSize.Small && (route |> List.contains nextCave)
+let partTwoFilter nextCave route = 
+    nextCave.Size = CaveSize.Small
+    && (route |> List.contains nextCave)
+    && (route 
+        |> List.filter (fun f -> f.Size = CaveSize.Small) 
+        |> List.groupBy (fun f -> f.Id) 
+        |> List.exists (fun f -> 
+            (snd f) |> List.length > 1))
+
 printfn "Advent of Code Day 12"
 
-let blaa = findPaths [] [] caves["start"]
-
-printfn "Answer 1: %d" blaa.Length
+let answer1 = findPaths partOneFilter [] [] caves["start"] 
+printfn "Answer 1: %d" answer1.Length
+let answer2 = findPaths partTwoFilter [] [] caves["start"] 
+printfn "Answer 2: %d" answer2.Length
