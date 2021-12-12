ed17052936b2db6469983294b15b4c9bb83f2d9b
Rinorragi <rinorragi@gmail.com>
2021-12-12 20:18:15 +0200

    Day 12 - Tidy up

--- day12.fsx
+++ day12.fsx
@@ -1,7 +1,7 @@
 #time
 open System
 
-type CaveSize =
+type CaveType =
     | Start
     | End
     | Large
@@ -9,7 +9,7 @@ type CaveSize =
 
 type Cave = {
     Id: string
-    Size: CaveSize
+    Size: CaveType
     Connections : string list
 }
 
@@ -23,13 +23,13 @@ let IsAllLower (caveName : string) =
 
 let solveCaveSize (caveName : string) =
     match caveName with
-    | "start" -> CaveSize.Start
-    | "end" -> CaveSize.End
+    | "start" -> Start
+    | "end" -> End
     | _ -> 
         if IsAllUpper caveName 
-        then CaveSize.Large
+        then Large
         elif IsAllLower caveName
-        then CaveSize.Small
+        then Small
         else raise(ArgumentException((sprintf "%s is mixed upper and lower case, huh what?" caveName)))
         
 let solveConnections (caveConnections: (string * string) list) (caveName: string) =
@@ -45,14 +45,11 @@ let cavesRaw =
         let caves = f.Split("-", StringSplitOptions.RemoveEmptyEntries)
         (caves[0], caves[1]))
 
-let caveNames = 
+let caves = 
     cavesRaw
     |> List.map (fun f -> [fst f; snd f])
     |> List.concat
     |> List.distinct
-
-let caves = 
-    caveNames
     |> List.map (fun f -> 
         f, {
         Id = f
@@ -64,17 +61,15 @@ let routeToString (route: Cave list) =
     route |> List.map (fun r -> r.Id) |> List.fold (+) "->"
 
 let filterNextCaves smallCaveFilter (routeResults: Cave list list) (route: Cave list) (nextCave : Cave) = 
-    if nextCave.Size = CaveSize.Start
-    then false
-    elif smallCaveFilter nextCave route
-    then false
-    elif routeResults |> List.map routeToString |> List.exists (fun f -> f.StartsWith(routeToString (route @ [nextCave])))
+    if nextCave.Size = Start 
+        || smallCaveFilter nextCave route
+        || routeResults |> List.map routeToString |> List.exists (fun f -> f.StartsWith(routeToString (route @ [nextCave])))
     then false
     else true
 
 let rec findPaths smallCaveFilter (routeResults: Cave list list) (route: Cave list) (currentCave : Cave)  = 
     let newRoute = route @ [currentCave]
-    if currentCave.Size = CaveSize.End
+    if currentCave.Size = End
     then  routeResults @ [newRoute]
     else 
         let pathsToContinue =
@@ -93,12 +88,12 @@ let rec findPaths smallCaveFilter (routeResults: Cave list list) (route: Cave li
         |> List.distinct
             
 
-let partOneFilter nextCave route = nextCave.Size = CaveSize.Small && (route |> List.contains nextCave)
+let partOneFilter nextCave route = nextCave.Size = Small && (route |> List.contains nextCave)
 let partTwoFilter nextCave route = 
-    nextCave.Size = CaveSize.Small
+    nextCave.Size = Small
     && (route |> List.contains nextCave)
     && (route 
-        |> List.filter (fun f -> f.Size = CaveSize.Small) 
+        |> List.filter (fun f -> f.Size = Small) 
         |> List.groupBy (fun f -> f.Id) 
         |> List.exists (fun f -> 
             (snd f) |> List.length > 1))
