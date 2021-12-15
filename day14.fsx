69a58e807bbdbf5e979e9973214cab1f0aca1b11
Rinorragi <rinorragi@gmail.com>
2021-12-15 06:30:38 +0200

    Day 14: Part 2

--- day14.fsx
+++ day14.fsx
@@ -12,58 +12,63 @@ let polymerFormulaRaw =
     System.IO.File.ReadAllText "./input/input_day14.txt" 
     |> (fun s -> s.Split(nl + nl, StringSplitOptions.RemoveEmptyEntries))
 
-let polymerTemplate = polymerFormulaRaw[0]
+let polymerTemplate = 
+    polymerFormulaRaw[0].ToCharArray() 
+    |> Array.pairwise 
+    |> Array.map (fun f -> [|fst f; snd f|] |> System.String)
+    |> Array.groupBy id
+    |> Array.map (fun f -> fst f, snd f |> Array.length |> int64)
+    |> Map.ofArray
 
 let pairInsertionRules = 
     polymerFormulaRaw[1].Split(nl, StringSplitOptions.RemoveEmptyEntries)
     |> Array.map(fun instruction -> 
         let instructionPair = instruction.Split(" -> ")
+        instructionPair[0], 
         {
             Pair = (instructionPair[0].ToCharArray()[0], instructionPair[0].ToCharArray()[1])
             Insertion = instructionPair[1].ToCharArray()[0]})
+    |> Map.ofArray
 
-let matchInsertionRule (chars : char array) (index : int) =
-    if index = chars.Length - 1 
-    then None
-    else 
-        let insertionRule = 
-            pairInsertionRules 
-            |> Array.tryFind (fun ir -> (fst ir.Pair) = chars[index] && (snd ir.Pair) = chars[index + 1])
-        match insertionRule with 
-        | None -> None
-        | Some(ir) -> Some(ir.Insertion)
+let upsertToMap (aMap: Map<'a,int64>) (key: 'a) (aValue: int64) =
+    match aMap.ContainsKey(key) with
+    | true ->  aMap |> Map.add key (aMap[key] + aValue)
+    | false -> aMap |> Map.add key aValue
 
-let getLeastCommonAndMostCommon (s: string) =
+let getLeastCommonAndMostCommon (aMap: Map<string,int64>) =
+    let charList = 
+        aMap 
+        |> Map.fold (fun (state: Map<char,int64>) key value -> 
+            let elem1State = upsertToMap state (key.ToCharArray()[0]) value
+            upsertToMap elem1State (key.ToCharArray()[1]) value
+        ) ([] |> Map.ofList)
+        |> Map.toList
     let mostCommon = 
-        s.ToCharArray()
-        |> Array.groupBy id
-        |> Array.map (fun (i, is) -> i, (is |> Seq.length))
-        |> Array.maxBy snd
+        charList
+        |> List.maxBy snd
     let leastCommon =
-        s.ToCharArray()
-        |> Array.groupBy id
-        |> Array.map (fun (i, is) -> i, (is |> Seq.length))
-        |> Array.minBy snd
+        charList
+        |> List.minBy snd
     (mostCommon, leastCommon)
 
-let printResult (s: string) (step: int) =
+let printResult (aMap: Map<string,int64>) (step: int) =
     let part = if step = 10 then 1 else 2
-    let (mostCommon, leastCommon) = getLeastCommonAndMostCommon s
-    printfn "Answer %d: Most common %c occurs %d times and least common %c occurs %d times which results %d" part (fst mostCommon) (snd mostCommon) (fst leastCommon) (snd leastCommon) (snd mostCommon - snd leastCommon)
+    let (mostCommon, leastCommon) = getLeastCommonAndMostCommon aMap
+    // Characters appears always in pairs, so we have double too much of them
+    let least = floor ((double)(snd leastCommon) / (double)2)
+    let most = ceil ((double)(snd mostCommon) / (double)2)
+    printfn "Answer %d: Most common %c occurs %f times and least common %c occurs %f times which results %f" part (fst mostCommon) most (fst leastCommon) least (most - least)
 
-let rec processInstructions (polymer: string) (step : int) =
-    printfn "%s: %d" (DateTime.Now.ToString "HH:mm:ss") step
-    let currentChars = polymer.ToCharArray()
-    let indexes = [0 .. (currentChars.Length - 1)]
+let rec processInstructions (polymer: Map<string,int64>) (step: int) =
     let newPolymer = 
-        indexes
-        |> List.fold(fun charArr i ->
-            match matchInsertionRule currentChars i with
-            | None -> charArr @ [currentChars[i]]
-            | Some(ir) -> charArr @ [currentChars[i]; ir]
-        ) []
-        |> Array.ofList
-        |> System.String
+        polymer
+        |> Map.fold (fun state key value -> 
+            let ir = pairInsertionRules[key]
+            let elem1 = [|fst ir.Pair; ir.Insertion|] |> System.String
+            let elem2 = [|ir.Insertion; snd ir.Pair|] |> System.String
+            let elem1State = upsertToMap state elem1 value 
+            upsertToMap elem1State elem2 value
+        ) ([] |> Map.ofList)
     if step = 10
     then 
         printResult newPolymer step
