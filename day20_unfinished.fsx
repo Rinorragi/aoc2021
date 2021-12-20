df2f2d4d6870768059592609dc3cdfb09aac923d
Rinorragi <rinorragi@gmail.com>
2021-12-20 13:34:59 +0200

    Day 20: part 1

--- day20_unfinished.fsx
+++ day20_unfinished.fsx
@@ -1,16 +1,137 @@
 #time
 open System
 
+type Pixel = {
+    Id : string
+    Row : int
+    Column : int
+    PixelValue : char
+    Binary : string
+    BinaryValue : int
+} 
+
+let charToBit (aChar : char) =
+    match aChar with 
+    | '#' -> '1'
+    | '.' -> '0'
+    | _ -> raise(ArgumentException("Invalid char"))
+
+let positionToBinaryString (grid : char array array) (maxRowIndex: int) (maxColumnIndex: int) (rowIndex : int) (columnIndex : int) (initialState : char) =
+    if rowIndex < 0 then charToBit initialState
+    elif rowIndex > maxRowIndex then charToBit initialState
+    elif columnIndex < 0 then charToBit initialState
+    elif columnIndex > maxColumnIndex then charToBit initialState
+    else charToBit (grid[rowIndex][columnIndex])
+
+
+let pixelToBinaryValue (grid : char array array) (maxRowIndex: int) (maxColumnIndex: int) (rowIndex : int) (columnIndex : int) (initialState : char) =
+    let topleft = positionToBinaryString grid maxRowIndex maxColumnIndex (rowIndex - 1) (columnIndex - 1) initialState
+    let top = positionToBinaryString grid maxRowIndex maxColumnIndex (rowIndex - 1) columnIndex initialState
+    let topright = positionToBinaryString grid maxRowIndex maxColumnIndex (rowIndex - 1) (columnIndex + 1) initialState
+    let left = positionToBinaryString grid maxRowIndex maxColumnIndex rowIndex (columnIndex - 1) initialState
+    let center = positionToBinaryString grid maxRowIndex maxColumnIndex rowIndex columnIndex initialState
+    let right = positionToBinaryString grid maxRowIndex maxColumnIndex rowIndex (columnIndex + 1) initialState
+    let botleft = positionToBinaryString grid maxRowIndex maxColumnIndex (rowIndex + 1) (columnIndex - 1) initialState
+    let bot = positionToBinaryString grid maxRowIndex maxColumnIndex (rowIndex + 1) columnIndex initialState
+    let botright = positionToBinaryString grid maxRowIndex maxColumnIndex (rowIndex + 1) (columnIndex + 1) initialState
+    [|topleft;top;topright;left;center;right;botleft;bot;botright|] |> System.String     
+
+let charToPixel (grid : char array array) (maxRowIndex: int) (maxColumnIndex: int) (rowIndex : int) (columnIndex : int) (initialState : char) =
+    let binaryString = pixelToBinaryValue grid maxRowIndex maxColumnIndex rowIndex columnIndex initialState
+    {
+        Id = sprintf "%d,%d" rowIndex columnIndex
+        Row = rowIndex
+        Column = columnIndex
+        PixelValue = grid[rowIndex][columnIndex]
+        Binary = binaryString
+        BinaryValue = binaryString |> (fun s -> Convert.ToInt32(s,2))
+    }
+
+let charArrayArrayToPixelArrayArray (grid : char array array) (initialState : char) =
+    let maxRowIndex = grid.Length - 1
+    let maxColumnIndex = grid[0].Length - 1
+    grid
+    |> Array.mapi (fun rowIndex row -> 
+        row |> Array.mapi (fun columnIndex value -> charToPixel grid maxRowIndex maxColumnIndex rowIndex columnIndex initialState)
+    )
+
+let toArray (arr: 'T [,]) = arr |> Seq.cast<'T> |> Seq.toArray
+
+let stretchImage (image : Pixel array array) (initialState : char) =
+    // True data seems to have pixel lit on (#) at
+    // ...
+    // ...
+    // ...
+    // And to lit off at 
+    // ### 
+    // ###
+    // ### 
+    // Handle "infinity" by enforcing pixel barrier around image each round and "play like that is the infinity"
+    // End result is either infinity or within those boundaries that are expanded this way
+    let initialRowLength = image.Length
+    let initialColumntLength = image[0].Length
+    let boundaryWidth = 3
+    let rowLengthChange = boundaryWidth * 2
+    [|0 .. initialRowLength + rowLengthChange - 1|]
+    |> Array.map (fun x -> 
+        Array.init (initialColumntLength + rowLengthChange) (fun y -> 
+            if (x < boundaryWidth 
+                || x > (initialRowLength + boundaryWidth - 1) 
+                || y < boundaryWidth 
+                || y > (initialColumntLength + boundaryWidth - 1))
+            then initialState
+            else (image[x - boundaryWidth][y - boundaryWidth]).PixelValue))
+    |> (fun f -> charArrayArrayToPixelArrayArray f initialState)
+
+let createOutputImage (image : Pixel array array) (enhancementAlgorithm: string) (initialState : char) = 
+    image
+    |> Array.map (fun x -> 
+        x |> Array.map (fun y -> enhancementAlgorithm[(image[y.Row][y.Column]).BinaryValue]))
+    |> (fun f -> charArrayArrayToPixelArrayArray f initialState)
+
+let printPixelArray (image : Pixel array array) =
+    image 
+    |> Array.map (fun row -> (row |> Array.map (fun col -> col.PixelValue)) |> System.String)
+    |> Array.map (printfn "%s") 
+    |> ignore
+
 printfn "Advent of Code Day 20"
 let nl = "\n"
 let imageScannerRawData = 
     System.IO.File.ReadAllText "./input/input_day20.txt" 
     |> (fun s -> s.Split(nl + nl, StringSplitOptions.RemoveEmptyEntries))
 
-let imageEnhancementAlgorithm = imageScannerRawData[0]
-let inputImage = 
+let inputImageRaw = 
     imageScannerRawData[1] 
     |> (fun s -> s.Split(nl, StringSplitOptions.RemoveEmptyEntries))
+    |> Array.map (fun s -> s.ToCharArray())
 
-printfn "%s" imageEnhancementAlgorithm
-printfn "%A" inputImage
+let imageEnhancementAlgorithm = imageScannerRawData[0]
+let inputImage = 
+    charArrayArrayToPixelArrayArray inputImageRaw '.' 
+    |> (fun f -> stretchImage f '.')
+printPixelArray inputImage
+
+let litResult = 
+    [1..2]
+    |> List.fold (fun acc currentRound ->
+            // Stupid hack to handle "infinity"
+        let initialState = 
+            if imageEnhancementAlgorithm[0] = '.'
+            then '.'
+            elif (imageEnhancementAlgorithm[0] = '#' && currentRound % 2 = 1)
+            then '#' // first round . -> #
+            elif imageEnhancementAlgorithm[imageEnhancementAlgorithm.Length - 1] = '.'  // second round # -> 
+            then '.'
+            else '#'
+        let midResult = createOutputImage acc imageEnhancementAlgorithm initialState
+        let largerPicture = stretchImage midResult initialState
+        printPixelArray largerPicture |> ignore
+        largerPicture
+    ) inputImage
+
+litResult 
+|> Array.map(fun row -> row |> Array.filter(fun s -> s.PixelValue = '#'))
+|> Array.concat
+|> Array.length
+|> printfn "Answer 1: %d"
